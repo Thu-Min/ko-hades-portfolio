54969544e28ea4cc4f7f57537a9267f5b6345442
Thu Min <minnt5637@gmail.com>
2023-09-19 11:04:55 +0630

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <section class="bg-center bg-no-repeat h-screen w-screen bg-[../../public/Artist Background.jpg] bg-gray-700 bg-blend-multiply bg-fixed">
+
+    <!-- <div id="default-carousel" class="relative w-full" data-carousel="slide">
+
+        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+
+            <div class="duration-100 ease-in-out" data-carousel-item>
+                <img src="https://blog.artweb.com/wp-content/uploads/2022/12/Van-Gogh-Starry-Night.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                <h1>hi</h1>
+            </div>
+
+            <div class="duration-100 ease-in-out" data-carousel-item>
+                <img src="https://images.artsmia.org/wp-content/uploads/2022/11/17112836/ext_202207080012-resized.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+            </div>
+        </div> 
+    </div> -->
+
+    <NavBar/>
+
+    <HomePage/>
+
+  </section>
+
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage, NavBar
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-
+        <!-- <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply"> -->
+            <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">I am Kyi Shwin Soe</h1>
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">and I am an Artist.</h1>
+                <p class="mb-10 text-md font-normal text-gray-300 lg:text-lg sm:px-16 lg:px-48">Hello there! I'm Kyi Shwin Soe, an artist who loves bringing imagination to life through colorful paintings. Welcome to my creative world!</p>
+                <!-- <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4"> -->
+                <div class="flex flex-col items-center justify-center">
+                    <p class="text-md font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">My Slogan</p>
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"/>
+                    </svg>
+                    <!-- <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                        Get started
+                        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                    <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                        Learn more
+                    </a>   -->
+                </div>
+            </div>
+        <!-- </section> -->
     </div>
 </template>
 
